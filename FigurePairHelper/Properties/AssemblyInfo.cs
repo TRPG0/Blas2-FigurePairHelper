11d6cc4fbe5cd042a3bcc15dcefd1494d36573c3
trevorleslie <80716066+TRPG0@users.noreply.github.com>
2025-07-30 19:51:15 -0600

    fix project files

--- FigurePairHelper/Properties/AssemblyInfo.cs
+++ FigurePairHelper/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using FigurePairHelper;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(FigurePairHelper.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonOptionalDependencies("Modding API", "0.1.0")]
+[assembly: MelonOptionalDependencies("Modding API", "2.0.0")]
